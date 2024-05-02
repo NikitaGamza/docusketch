42c08ee8a93468234d425dcd16e6a22592886fe1
Gamza <gamza-ns@mail.ru>
2024-05-02 16:01:31 +0300

    fix: console

--- src/app/components/Random/Random.tsx
+++ src/app/components/Random/Random.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Profiler, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import style from './Random.module.scss';
@@ -6,26 +6,35 @@ import style from './Random.module.scss';
 export default function Random() {
   const [iconsArr, setIconsArr] = useState<Array<any>>(Object.entries(fas));
   const [randInd, setRandInd] = useState<number>(0);
-  useEffect(() => {
-    console.log(iconsArr);
-  }, []);
   function getRand() {
     setRandInd(Math.floor(Math.random() * iconsArr.length));
   }
+  function onRender(
+    id: string,
+    phase: 'mount' | 'apdate',
+    actualDuration: number,
+    baseDuration: number,
+    startTime: number,
+    commitTime: number
+  ) {
+    console.log(`Actual duralation time: ${actualDuration}`);
+    console.log(`Base duralation time: ${baseDuration}`);
+  }
   return (
     <div className={style.rand}>
-      <FontAwesomeIcon
-        icon={iconsArr[randInd][1]}
-        className={style.rand__display}
-        data-testid="iconBox"
-        title={'ht'}
-      />
-      <button
-        className={style.rand__btn}
-        onClick={() => setTimeout(getRand, 3000)}
-      >
-        Get random icon
-      </button>
+      <Profiler id="random" onRender={onRender}>
+        <FontAwesomeIcon
+          icon={iconsArr[randInd][1]}
+          className={style.rand__display}
+          data-testid="iconBox"
+        />
+        <button
+          className={style.rand__btn}
+          onClick={() => setTimeout(getRand, 3000)}
+        >
+          Get random icon
+        </button>
+      </Profiler>
     </div>
   );
 }
